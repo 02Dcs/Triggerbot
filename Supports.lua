7948d0321f2915fe0f0afb68c40be2536b2360e3
02Dcs <95237577+02Dcs@users.noreply.github.com>
2023-08-02 22:21:05 +0200

    Create Supports.lua

---
+++ Supports.lua
@@ -0,0 +1,16 @@
+local supports = {};
+
+local games = {
+    ["621129760"] = "Kat",
+    ["6514923093"] = "Realistic-Guns",
+}
+
+local function supports.CheckGm()
+local x = tostring(game.PlaceId)
+if games[x] then
+    print(games);
+else
+   game.Players:Kick('Trigger-Bot Does not support that game!')
+end; end
+
+return supports
